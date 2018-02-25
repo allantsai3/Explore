f252abaa783280e462768f62aad8b617d05b570d
Allan Tsai <allantsai3@gmail.com>
2018-02-24 17:11:57 -0800

    player coordinates and movement changes

--- gameloop.py
+++ gameloop.py
@@ -10,16 +10,20 @@ tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE, INITIAL_GAME_F
 #limiting FPS for non turn based
 tcod.sys_set_fps(LIMIT_FPS)
 
+#Player information
+player_x = SCREEN_WIDTH // 2
+player_y = SCREEN_HEIGHT // 2
+player_coord = {'x': player_x, 'y': player_y}
 
 #Main Loop of the game
 while not tcod.console_is_window_closed():
     tcod.console_set_default_foreground(0, tcod.white)
-    tcod.console_put_char(0, player_x, player_y, 'x', tcod.BKGND_NONE)
+    tcod.console_put_char(0, player_coord['x'], player_coord['y'], 'x', tcod.BKGND_NONE)
 
     tcod.console_flush()
-    tcod.console_put_char(0, player_x, player_y, ' ', tcod.BKGND_NONE) #erases the last position
+    tcod.console_put_char(0, player_coord['x'], player_coord['y'], ' ', tcod.BKGND_NONE) #erases the last position
 
     #key handling
-    exit = handle_keys()
+    exit = handle_keys(player_coord)
     if exit:
         break
--- player.py
+++ player.py
@@ -2,10 +2,8 @@ from const import *
 import tcod
 
 #Player information
-player_x = SCREEN_WIDTH // 2
-player_y = SCREEN_HEIGHT // 2
 
-def handle_keys():
+def handle_keys(coord):
     # key = tcod.console_wait_for_keypress(True)
     key = tcod.console_check_for_keypress()
     if key.vk == tcod.KEY_ENTER and key.lalt:
@@ -16,15 +14,14 @@ def handle_keys():
     elif key.vk == tcod.KEY_ESCAPE:
         return True
 
-    global player_y, player_x
     if tcod.console_is_key_pressed(tcod.KEY_UP):
-        player_y = player_y - 1
+        coord['y'] = coord['y'] - 1
 
     elif tcod.console_is_key_pressed(tcod.KEY_DOWN):
-        player_y = player_y + 1
+        coord['y'] = coord['y'] + 1
 
     elif tcod.console_is_key_pressed(tcod.KEY_LEFT):
-        player_x = player_x - 1
+        coord['x'] = coord['x'] - 1
 
     elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
-        player_x = player_x + 1
+        coord['x'] = coord['x'] + 1
